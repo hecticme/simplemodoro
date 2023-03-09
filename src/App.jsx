211e1c02820480345581a18712f1f88c4f9b675d
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-03-09 16:16:12 +0700

    add set timer logic and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
 import { useState } from "react";
 import { Modal } from "./Modal";
+import {
+  PlayIcon,
+  PauseIcon,
+  ArrowPathRoundedSquareIcon,
+} from "@heroicons/react/24/solid";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+  const [time, setTime] = useState({ sessionTime: 25 * 60, breakTime: 5 * 60 });
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  };
 
   return (
-    <div
-      className="flex h-screen flex-col items-center justify-center gap-4"
-      onClick={() => {
-        setIsModalOpen((prev) => !prev);
-      }}
-    >
-      <h1 className="text-8xl font-bold">25:00</h1>
-      <div className="h-2 w-[20%] rounded-full bg-gray-300">
-        <div className="h-2 w-[10%] rounded-full bg-gray-800"></div>
+    <div className="flex h-screen select-none flex-col items-center justify-center gap-4 p-4">
+      <h1 className="text-6xl font-bold sm:text-7xl md:text-8xl">
+        {formatTime(time.sessionTime)}
+      </h1>
+      <div className="mb-4 flex gap-2">
+        <div
+          className="flex aspect-square w-10 cursor-pointer items-center justify-center rounded-full bg-gray-900 hover:bg-gray-700 hover:shadow-md hover:shadow-slate-300"
+          onClick={() => {
+            setIsPaused((prev) => !prev);
+          }}
+        >
+          {isPaused ? (
+            <PlayIcon className="w-1/2 text-white" />
+          ) : (
+            <PauseIcon className="w-1/2 text-white" />
+          )}
+        </div>
+        <div className="flex aspect-square w-10 cursor-pointer items-center justify-center rounded-full bg-gray-900 hover:bg-gray-700 hover:shadow-md hover:shadow-slate-300">
+          <ArrowPathRoundedSquareIcon className="w-1/2 text-white" />
+        </div>
       </div>
+
       <div className="flex flex-wrap gap-2">
         <div
           className="cursor-pointer rounded bg-gray-900 py-2 px-4 text-white transition-colors hover:bg-gray-700 hover:shadow-md hover:shadow-slate-300"
-          onClick={() => {
+          onClick={(e) => {
             e.stopPropagation();
             setIsModalOpen((prev) => !prev);
           }}
@@ -26,7 +55,28 @@ function App() {
           Choose Duration.
         </div>
       </div>
-      <Modal isModalOpen={isModalOpen} />
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h2 className="mt-4 text-xl font-bold sm:text-2xl md:text-3xl">
+          Today's Goal
+        </h2>
+        <div className="h-2 w-full rounded-full bg-gray-300">
+          <div className="h-2 w-[10%] rounded-full bg-gray-800"></div>
+        </div>
+      </div>
+      <div
+        className={`pointer-events-none fixed h-full w-full bg-gray-900 opacity-30 ${
+          isModalOpen ? "pointer-events-auto block" : "hidden"
+        }`}
+        onClick={() => {
+          setIsModalOpen(false);
+        }}
+      ></div>
+      <Modal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        time={time}
+        setTime={setTime}
+      />
     </div>
   );
 }
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,11 +1,86 @@
-export function Modal({ isModalOpen }) {
+import { XMarkIcon } from "@heroicons/react/24/solid";
+
+export function Modal({ isModalOpen, setIsModalOpen, time, setTime }) {
+  function handleChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    // Revert to maximum number if the value exceed that.
+    if (name == "sessionTime" && value > 240) {
+      setTime((prev) => {
+        return {
+          ...prev,
+          [name]: 240 * 60,
+        };
+      });
+    } else if (name == "breakTime" && value > 30) {
+      setTime((prev) => {
+        return {
+          ...prev,
+          [name]: 30 * 60,
+        };
+      });
+    } else {
+      // Else set the value normally.
+      setTime((prev) => {
+        return {
+          ...prev,
+          [name]: value * 60,
+        };
+      });
+    }
+  }
+
   return (
     <div
       className={`fixed ${
-        isModalOpen ? "flex" : "hidden"
-      } h-[70%] w-1/2 items-center justify-center rounded-md bg-slate-900`}
+        isModalOpen ? "scale-100" : "scale-0"
+      } flex h-[70%] w-[90%] flex-col items-center justify-center gap-6 rounded-md bg-slate-900 text-slate-100 transition-transform duration-300 sm:w-[70%] xl:w-1/2`}
     >
-      <h1>Set your timer</h1>
+      <div className="absolute top-2 right-2 aspect-square h-6 cursor-pointer rounded-full bg-slate-200 transition-transform hover:scale-110 hover:bg-slate-100">
+        <XMarkIcon
+          className="scale-75 text-gray-900"
+          onClick={() => {
+            setIsModalOpen(false);
+          }}
+        />
+      </div>
+      <h1 className="text-xl font-bold sm:text-2xl">Set your timer</h1>
+      <div className="flex w-[70%] flex-col gap-4">
+        <label htmlFor="sessionTime" className="text-sm sm:text-base">
+          Session Time
+        </label>
+        <input
+          type="number"
+          id="sessionTime"
+          name="sessionTime"
+          min="10"
+          max="240"
+          step="5"
+          value={(time.sessionTime / 60).toString()}
+          className="w-full rounded bg-slate-100 p-1 text-sm text-gray-900 sm:p-2 sm:text-base"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+      </div>
+      <div className="flex w-[70%] flex-col gap-4">
+        <label htmlFor="breakTime" className="text-sm sm:text-base">
+          Break Time
+        </label>
+        <input
+          type="number"
+          id="breakTime"
+          name="breakTime"
+          min="5"
+          max="30"
+          step="5"
+          value={(time.breakTime / 60).toString()}
+          className="w-full rounded bg-slate-100 p-1 text-sm text-gray-900 sm:p-2 sm:text-base"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+      </div>
     </div>
   );
 }
